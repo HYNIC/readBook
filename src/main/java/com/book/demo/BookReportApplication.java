c4d1c762fd2a73cb5039137977d2b693a304a91b
hynie <hynie@HYUN>
2022-03-10 23:59:27 +0900

    test

---
+++ src/main/java/com/book/demo/BookReportApplication.java
@@ -0,0 +1,13 @@
+package com.book.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReportApplication.class, args);
+	}
+
+}
